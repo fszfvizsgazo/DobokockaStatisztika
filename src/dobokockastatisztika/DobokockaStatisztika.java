15a35abc0dfdc714f8a272f6ba73eeaf1f2c3fba
Csatáry Béla <FSZFvizsgazo15@gmail.com>
2023-10-11 11:54:02 +0200

    First commit

--- src/dobokockastatisztika/DobokockaStatisztika.java
+++ src/dobokockastatisztika/DobokockaStatisztika.java
@@ -4,8 +4,9 @@
  */
 package dobokockastatisztika;
 
+import java.util.Scanner;
+
 /**
- *
  * @author Mr.KaraszekZoltán
  */
 public class DobokockaStatisztika {
@@ -13,20 +14,52 @@ public class DobokockaStatisztika {
     /**
      * @param args the command line arguments
      */
-    String sorozat="123146166661611115555152444425";
+    String sorozat = "123146166661611115555152444425";
+
     public static void main(String[] args) {
         // TODO code application logic here
-        Tesztesetek t=new Tesztesetek();
+        Tesztesetek t = new Tesztesetek();
         t.setUpAll();
         t.setUpStreams();
         t.tesztSzam();
         t.tesztHossz();
         t.restoreStreams();
     }
-    int beolvas(){
-        return 6;
+
+    int beolvas() {
+
+        Scanner sc = new Scanner(System.in);
+        int dobas;
+        dobas = sc.nextInt();
+        while (dobas < 1 || dobas > 6) {
+            dobas = sc.nextInt();
+        }
+        return dobas;
     }
-    void leghosszabb(int szam){
-        System.out.printf("%s: %d","6666",4);
+
+    void leghosszabb(int szam) {
+        int maxHossz = 0;
+        int aktualHossz = 0;
+        boolean sorozatE = false;
+        for (int i = 0; i < sorozat.length(); i++) {
+            if (sorozat.charAt(i) == szam) {
+                if (sorozatE) {
+                    aktualHossz++;
+                } else {
+                    aktualHossz = 1;
+                    sorozatE = true;
+                }
+            } else {
+                if (sorozatE && (aktualHossz > maxHossz)) {
+                    maxHossz = aktualHossz;
+                }
+                sorozatE = false;
+            }
+        }
+        String ered = "";
+        for (int i = 0; i < maxHossz; i++) {
+            ered += String.valueOf(szam);
+        }
+        System.out.printf("%s: %d", ered, maxHossz);
     }
 }
